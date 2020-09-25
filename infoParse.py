dd473a06f987572aa0a14d7a88562dc675fe724e
Eric Tong <etong8128@gmail.com>
2020-09-25 16:58:54 -0700

    allMeetings function

--- infoParse.py
+++ infoParse.py
@@ -14,7 +14,6 @@ def longForm(location):
     tokens = location.split()
     if tokens[0] in abbreviations:
         tokens[0] = abbreviations[tokens[0]]
-    print(tokens)
     return " ".join(tokens)
 
 # Placeholders for days of the week
@@ -31,31 +30,54 @@ def parseTime(dow, time):
 
 # Represents a course
 class Course:
-    def __init__(self, courseId, title, days, start, end):
+    def __init__(self, courseId, title, days, start, end, room, instructor):
         self.courseId = courseId
         self.title = title
         self.meetingTimes = []
         for day in days:
-            meeting = ClassPeriod(courseId, parseTime(day, start), parseTime(day, end))
+            meeting = ClassPeriod(courseId, parseTime(day, start), parseTime(day, end), room)
             self.meetingTimes.append(meeting)
+        self.room = room
+        self.instructor = instructor
     
     def displayInfo(self):
-        print("{} {}".format(self.courseId, self.title))
-        print(self.meetingTimes)
+        ret = "{} {}\n".format(self.courseId, self.title)
+        ret += "Meetings: {}".format(self.meetingTimes)
+        return ret
         
+def extractDOW(dateObj):
+    return dateObj.strftime("%A")
+
+def goodDateFormat(dateObj):
+    return dateObj.strftime("%H:%M")
+
 # Represents a Class Period
 class ClassPeriod:
-    def __init__(self, classId, start, end):
+    def __init__(self, classId, start, end, room):
         self.classId = classId
         self.start = start
         self.end = end
+        self.room = room
 
     def __repr__(self):
-        return "[{}: {} to {}]".format(self.classId, self.start, self.end)
+        return "{} {} to {} @ {}".format(extractDOW(self.start), goodDateFormat(self.start), goodDateFormat(self.end),
+            longForm(self.room))
+
+# Takes a list of classes and returns all meetings
+# Also sorts by time
+def allMeetings(courses):
+    meetings = []
+    for course in courses:
+        for meeting in course.meetingTimes:
+            meetings.append(meeting)
+    meetings.sort(key=lambda meeting: meeting.start)
+    return meetings
 
 parseTime("F", "12:50PM")
 
 print(longForm("PH 101"))
 
-course1 = Course("15122", "Principles of Imperative Computing", "TR", "08:00AM", "09:20AM")
-course1.displayInfo()
+course1 = Course("15122", "Principles of Imperative Computing", "TR", "08:00AM", "09:20AM", "BH 5001", "Cervesato")
+course2 = Course("15150", "Principles of Functional Programming", "TR", "11:40AM", "01:00PM", "CMU REMOTE", "Brookes")
+print(course1.displayInfo())
+print(allMeetings([course1, course2]))
